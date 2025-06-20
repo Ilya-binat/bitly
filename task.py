515af4eb99fc398930f4c19c6875083d0a74d8f0
Ilya-bunat <ilya.binat15@gmail.com>
2025-06-20 21:28:10 +0300

    добавлены задачи

---
+++ task.py
@@ -0,0 +1,50 @@
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Гав-гав!")
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+    
+class BankAccount:
+    def __init__(self):
+        self.balance = 0
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Счет пополнен на {amount}. Текущий баланс: {self.balance}")
+        else:
+            print("Сумма пополнения должна быть положительной.")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Недостаточно средств для снятия.")
+        elif amount <= 0:
+            print("Сумма снятия должна быть положительной.")
+        else:
+            self.balance -= amount
+            print(f"Снято {amount}. Текущий баланс: {self.balance}")
+
+
+class Animal:
+    def eat(self):
+        print("Животное ест.")
+
+    def sleep(self):
+        print("Животное спит.")
+
+class Cat(Animal):
+    def meow(self):
+        print("Мяу!")
